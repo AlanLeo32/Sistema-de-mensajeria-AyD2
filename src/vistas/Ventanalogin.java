d96ee4fbf15886ea9c44d9ffeae20b77156f0568
Onti289 <ontiveroslucas1@gmail.com>
2025-04-12 17:48:51 -0300

    a

--- src/vistas/Ventanalogin.java
+++ src/vistas/Ventanalogin.java
@@ -18,6 +18,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
